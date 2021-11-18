6dc27c38da5d4b0bec508bd0110a86b1119b8557
moraes-luci <lucineia.rmoraes@gmail.com>
2021-11-18 13:07:51 -0300

    Criado atributos complexo, filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=32):
+    def __init__(self, *filhos, nome=None, idade=32):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar (self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Daniel')
-    print(p.cumprimentar())
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.nome)
-    p.nome = 'Lucineia'
-    print(p.nome)
-    print(p.idade)
+    lucineia = Pessoa(nome='lucineia')
+    daniel = Pessoa(lucineia,nome='Daniel')
+    print(daniel.cumprimentar())
+    print(Pessoa.cumprimentar(daniel))
+    print(id(daniel))
+    print(daniel.nome)
+    print(daniel.idade)
+    for filho in daniel.filhos:
+        print(filho.nome)
