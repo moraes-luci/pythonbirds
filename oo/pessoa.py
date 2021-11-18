7f0691065d1f9f8a488be17b318c6a5d4a3a9c2b
moraes-luci <lucineia.rmoraes@gmail.com>
2021-11-18 12:41:14 -0300

    Criado atributos de instancia , nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=32):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar (self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Daniel')
     print(p.cumprimentar())
     print(Pessoa.cumprimentar(p))
     print(id(p))
+    print(p.nome)
+    p.nome = 'Lucineia'
+    print(p.nome)
+    print(p.idade)
